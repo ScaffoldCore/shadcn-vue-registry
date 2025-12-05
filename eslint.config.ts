ab06fd7e462eaf657ffdad2e75db0da3415d5e64
lonewolfyx <877085893@qq.com>
2025-12-05 23:29:46 +0800

    refactor(eslint): refactor the ESLint configuration file into TypeScript format

---
+++ eslint.config.ts
@@ -0,0 +1,29 @@
+import type { Linter } from 'eslint'
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+import yamlParser from 'yaml-eslint-parser'
+
+const config = antfu({
+    type: 'lib',
+    stylistic: {
+        indent: 4,
+        quotes: 'single',
+    },
+    rules: {
+        'no-console': 'off',
+        'node/prefer-global/process': 'off',
+        'ts/explicit-function-return-type': 'off',
+        'vue/block-order': 'off',
+        'vue/valid-template-root': 'off',
+    },
+}, {
+    files: ['**/*.yml', '**/*.yaml'],
+    languageOptions: {
+        parser: yamlParser,
+    },
+    rules: {
+        'yaml/indent': ['error', 2],
+    },
+}) as Linter.Config
+
+export default config
