0ed42f47219734861f1ac4ff5949c99e7ca03660
lonewolfyx <877085893@qq.com>
2025-12-10 15:42:18 +0800

    refactor(config): optimizes the tsdown configuration structure and adjusts the tsconfig compilation options

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-    entry: ['src/cli.ts', 'src/index.ts'],
-    dts: true,
-    clean: true,
-    sourcemap: true,
-})
+export default defineConfig([
+    {
+        entry: 'src/cli.ts',
+        dts: false,
+    },
+    {
+        entry: 'src/index.ts',
+        dts: true,
+        platform: 'node',
+    },
+])
